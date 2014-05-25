a4b9e6540d4848c7a064c4b2305f672764bcef20
Bryan Sloan <bryan.datascience@outlook.com>
2014-05-25 09:52:43 -0400

    Programming Assignment 2 complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
+## cachematrix.R
 
-## Write a short comment describing this function
+## Function 'makeCacheMatrix' creates a matrix object that can cache
+## its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+## Function 'cacheSolve' computes the inverse of the matrix returned by
+## makeCacheMatrix.  If the inverse has been calculated, then cacheSolve will
+## retrieve the inverse for the cache.
 
+
+## Function to create a special 'matrix' object that can cache its inverse.
+
+makeCacheMatrix <- function(x= matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
 
-## Write a short comment describing this function
+## Function to compute the inverse of the special 'matrix' returned by
+## makeCacheMatrix. If the inverse has already been calculated (and the
+## matrix has not changed), then the cacheSolve will retrieve the inverse
+## from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
+
+## Test Cases
+##
+## Load the procedure
+## > source("cachematrix.R")
+##
+## Create a matrix
+## > c <- makeCacheMatrix(matrix(1:4,2,2))
+##
+## Display the matrix you just created
+## > c$get()
+##
+## Generate the inverse of the matrix
+## cacheSolve(c)
+##
+## Try to generate the inverse a second time.  cacheSolve returns cached data
+## cacheSolve(c)
+##
+## End of cachematrix.R
